a047eb4dd1dea89b418cee1e75e4cd9a50e98578
Desiah Barnett <desiahbarnett@gmail.com>
2024-01-10 07:56:59 -0500

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+  }
+  
+  // 4) Define the mondayWork function expression
+  const mondayWork = function(activity) {
+    if (activity === undefined) {
+      return "This Monday, I will go to the office.";
+    } else {
+      return `This Monday, I will work from home.`;
+    }
+  }
+  
+  // 7) Define the wrapAdjective function
+  function wrapAdjective(flair = "*") {
+    return function(adjective) {
+      return `You are ${flair}${adjective}${flair}!`;
+    }
+  }
+  
+  // Export the functions if needed
+  // module.exports = { saturdayFun, mondayWork, wrapAdjective };
